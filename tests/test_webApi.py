6c7ba4d5a9c9821fcb2637c1a8a90beb769b25e0
Freeman Man <freeman@travelloapp.com>
2020-11-03 10:26:05 +1000

    BS-1565 remove import app from test

--- tests/test_webApi.py
+++ tests/test_webApi.py
@@ -1,14 +1,15 @@
 
 import json
-import app
 from app import create_app
 
 app = create_app('test')
+
 
 def test_health():
     with app.test_client() as c:
         response = c.get('/health')
         assert response.status_code == 200
+
 
 def test_NonPrank():
     with app.test_client() as c:
@@ -18,6 +19,7 @@ def test_NonPrank():
             "/nonlinear/personal_rank", json=json_data)
         assert response.status_code == 200
 
+
 def test_NonGrank():
     with app.test_client() as c:
         with open('unit_test/g_data.json') as f:
@@ -25,5 +27,4 @@ def test_NonGrank():
         # pdb.set_trace()
         response = c.post(
             "/nonlinear/global_rank", json=json_data)
-        print(response)
         assert response.status_code == 200
